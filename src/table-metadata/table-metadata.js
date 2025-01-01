3f1ae7566f28b139110f525b68133b592945dd5a
mistakia <1823355+mistakia@users.noreply.github.com>
2025-01-01 18:54:37 -0500

    fix: table metadata time display

--- src/table-metadata/table-metadata.js
+++ src/table-metadata/table-metadata.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { format } from 'timeago.js'
+
 import PropTypes from 'prop-types'
 
 import './table-metadata.styl'
@@ -9,11 +11,12 @@ export default function TableMetadata({ created_at }) {
   const fifteen_minutes = 15 * 60 * 1000
   const time_diff = Date.now() - created_at
 
-  if (time_diff > fifteen_minutes) return null
+  if (time_diff < fifteen_minutes) return null
 
   return (
     <div className='table-metadata'>
-      View calculated at {new Date(created_at).toLocaleString()}
+      View calculated {format(created_at)} (
+      {new Date(created_at).toLocaleString()})
     </div>
   )
 }
